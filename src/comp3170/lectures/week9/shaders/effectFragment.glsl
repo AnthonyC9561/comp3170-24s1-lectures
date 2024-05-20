4ba5210b92b0377e0ef5e9b96e32e613523f8da3
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-05-20 11:28:40 +1000

    Fixed dithering

--- src/comp3170/lectures/week9/shaders/effectFragment.glsl
+++ src/comp3170/lectures/week9/shaders/effectFragment.glsl
@@ -7,25 +7,29 @@ in vec2 v_texcoord;	// UV
 layout(location = 0) out vec4 o_colour;
 
 const float NBUCKETS = 8.;
+const float SCALE = 10.;  // renderTexture pixels to screen pixels
 
 void main() {
 	vec3 c = texture(u_texture, v_texcoord).rgb;
-
-	// [0 .. 8]	
-	c = c * NBUCKETS;
 	
-	vec2 p = gl_FragCoord.xy;
-	float parity = mod(p.x + p.y, 2); 
-			
+	// map into twice as many buckets including 0.5 levels
+	// [0, 0.5, 1, 1.5, ..., 7.5, 8]	
+	c = round(c * NBUCKETS * 2) / 2;
+	
+	// get coordinates in rendertexture screenspace coords   
+	ivec2 p = ivec2(gl_FragCoord.xy / SCALE);
+	float parity = mod(p.x + p.y,  2.); 
+
 	if (parity == 0) {
-		// even pixel -> round up
+		// even pixel -> round 0.5 buckets up
 		c = ceil(c);
 	}
 	else {
-		// odd pixel -> rodown down
+		// odd pixel -> round 0.5 buckets down
 		c = floor(c);
 	}
 	
+	// convert back to range [0 ... 1]
 	c = c / NBUCKETS;
 			
     o_colour = vec4(c, 1);
