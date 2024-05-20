bc80705d4f5c8356cb4ba95028b507e57643d536
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-05-20 13:26:40 +1000

    Dithering should be done in intensity space, not brightness

--- src/comp3170/lectures/week9/shaders/effectFragment.glsl
+++ src/comp3170/lectures/week9/shaders/effectFragment.glsl
@@ -8,9 +8,24 @@ layout(location = 0) out vec4 o_colour;
 
 const float NBUCKETS = 8.;
 const float SCALE = 10.;  // renderTexture pixels to screen pixels
+const vec3 GAMMA = vec3(2.2); // (2.2, 2.2, 2.2)
+
+vec3 toIntensity(vec3 brightness) {
+	// I = B ^ 2.2 
+	return pow(brightness, GAMMA);
+}
+
+vec3 toBrightness(vec3 intensity) {
+	// B = I ^ 1/2.2
+	return pow(intensity, 1./GAMMA);
+}
+
 
 void main() {
 	vec3 c = texture(u_texture, v_texcoord).rgb;
+	
+	// Dithering should be done in intensity space
+	c = toIntensity(c);
 	
 	// map into twice as many buckets including 0.5 levels
 	// [0, 0.5, 1, 1.5, ..., 7.5, 8]	
@@ -31,6 +46,9 @@ void main() {
 	
 	// convert back to range [0 ... 1]
 	c = c / NBUCKETS;
+	
+	// convert back to brightness for output
+	c = toBrightness(c);
 			
     o_colour = vec4(c, 1);
 }
